762f6712b5b30bdb4e9606a2b864209a542a4786
macars1990 <m.serraninho@campus.fct.unl.pt>
2017-05-30 13:06:27 +0100

    ..

--- io/altar/jseproject/textinterface/TextInterface.java
+++ io/altar/jseproject/textinterface/TextInterface.java
@@ -3,7 +3,7 @@ package io.altar.jseproject.textinterface;
 import java.util.Scanner;
 
 public class TextInterface {
-	public static void main(String[] args) {
+	public static void mainMenu() {
 		System.out.println("Por favor selecione uma das seguintes opções:\n"+
 		"1) Listar produtos\n"+
 		"2) Listar prateleiras\n"+ 
